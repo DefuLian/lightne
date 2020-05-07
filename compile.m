95502a7f43d6173a0cf17b382c97f935a91c37cf
Defu Lian <dove.ustc@gmail.com>
2020-05-07 10:30:16 +0800

    add platform dependent codes

--- compile.m
+++ compile.m
@@ -3,7 +3,7 @@ mex -largeArrayDims beam_search.cpp
 cd AQ/utils;
 mex -largeArrayDims euc_nn_mex.cc
 mex -largeArrayDims kmeans_iter_mex.cc
-cd ..
+cd ../..
 
 cd netsmf;
 mex -largeArrayDims deepwalk_mex.cc BinaryGraphWalker.cc WeightGraphWalker.cc GraphWalker.cc; 
